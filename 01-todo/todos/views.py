dffc5bc54bf83d0a985aa8087fcb8de0da639870
Ahyar <ahyar@asaren.ai>
2025-11-21 16:22:17 +0700

    Initial iteration

---
+++ 01-todo/todos/views.py
@@ -0,0 +1,30 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.shortcuts import redirect, get_object_or_404
+from .models import Todo
+
+class TodoListView(ListView):
+    model = Todo
+    template_name = 'todos/todo_list.html'
+    context_object_name = 'todos'
+    ordering = ['-created_at']
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title', 'description', 'due_date']
+    success_url = reverse_lazy('todo_list')
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title', 'description', 'due_date', 'completed']
+    success_url = reverse_lazy('todo_list')
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy('todo_list')
+
+def complete_todo(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    todo.completed = True
+    todo.save()
+    return redirect('todo_list')
